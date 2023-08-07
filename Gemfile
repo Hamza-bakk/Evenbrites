6fb76c0c9aa5d7ee3c59eb4d699ec97e95c48606
Hamza <bakkar.hamza92@gmail.com>
2023-08-07 17:39:48 +0200

    Deuxieme branche

--- Gemfile
+++ Gemfile
@@ -72,3 +72,5 @@ group :test do
 end
 
   gem 'faker'
+
+  gem "letter_opener", group: :development
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Attendance.destroy_all
 User.destroy_all
 Event.destroy_all
+
 
 users = []
 
